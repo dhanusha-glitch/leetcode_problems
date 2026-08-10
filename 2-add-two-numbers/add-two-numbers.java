2aedb744bcd71cc36ca32a68e41b201b08fd2d2e
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-10 11:55:34 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (58.26%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -9,16 +9,16 @@ class Solution {
                 sum += l1.val;
                 l1 = l1.next;
             }
-            if(l2 != null){ // adding l2 to our sum & moving l2
+            if(l2 != null){ 
                 sum += l2.val;
                 l2 = l2.next;
             }
-            sum += carry; // if we have carry then add it into our sum
-            carry = sum/10; // if we get carry, then divide it by 10 to get the carry
-            ListNode node = new ListNode(sum % 10); // the value we'll get by moduloing it, will become as new node so. add it to our list
-            curr.next = node; // curr will point to that new node if we get
-            curr = curr.next; // update the current every time
+            sum += carry; 
+            carry = sum/10; 
+            ListNode node = new ListNode(sum % 10); 
+            curr.next = node; 
+            curr = curr.next; 
         }
-        return dummy.next; // return dummy.next bcz, we don't want the value we have consider in it intially!!
+        return dummy.next; 
     }
 }
