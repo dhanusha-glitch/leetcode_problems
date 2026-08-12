e8dcd7d84e128dc864d7e774aad07de8363727b2
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-12 14:57:38 +0530

    Time: 241 ms (64.11%) | Memory: 21.6 MB (47.68%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,20 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        if not grid: return 0
-        r, c = len(grid), len(grid[0])
-        visited = [[False for _ in range(c)] for _ in range(r)]
-
-        def dfs(i, j):
-            if i < 0 or i >= r or j < 0 or j >= c or grid[i][j] == '0' or visited[i][j]:
-                return
-            visited[i][j] = True
-            dfs(i + 1, j)
-            dfs(i - 1, j)
-            dfs(i, j + 1)
-            dfs(i, j - 1)
-
+class Solution:
+    def dfs(self, grid, i, j):
+        m, n = len(grid), len(grid[0])
+        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == '0':
+            return
+        grid[i][j] = '0'
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
         count = 0
-        for i in range(r):
-            for j in range(c):
-                if not visited[i][j] and grid[i][j] == '1':
-                    dfs(i, j)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
                     count += 1
+                    self.dfs(grid, i, j)
         return count
