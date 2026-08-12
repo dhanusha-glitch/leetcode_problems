6a47ec7df4e8fecfe0afa69a6370b50addccde9d
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-12 09:38:24 +0530

    Time: 63 ms (63.26%) | Memory: 149.1 MB (99.16%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
+    Solution(){ // constructor to be run before the solution --> lower time complexity
+        // Fast input
+        ios::sync_with_stdio(false); std::cin.tie(nullptr);
+    }
+    unordered_map<int , int> freq_mp; // prefered rather than ordered map as we do not need sorting
+    
     int maxSubarrayLength(vector<int>& nums, int k) {
-        int n=nums.size();
-        int l=0,r=0,len=0;
-        unordered_map<int,int>mpp;
-        while(r<n)
-        {
-            mpp[nums[r]]++;
-            while(mpp[nums[r]]>k)
-            {
-                mpp[nums[l]]--;
-                if(mpp[nums[l]]==0) mpp.erase(nums[l]);
-                l++;
+         
+        int sz = nums.size(), // size            
+        max_size = 0, // max size for window
+        head = 0 , tail = 0; // pointers used in sliding window
+        
+        while( head < sz && tail <=head ){ // 
+            freq_mp[nums[head]]++; // character apeared here
+            
+            if(freq_mp[nums[head]] <= k){ 
+                max_size = max(max_size , (head - tail + 1)); // head - tail + 1 --> size of the window
+                head++; // increasing window size from head [right]
+            } else{ // decreaseing window size from tail [left]
+                while(freq_mp[nums[head]] > k){ // keep decrease until a size with good repition
+                    freq_mp[nums[tail]]--;
+                    tail++; // moving left pointer to the right one step
+                }
+                max_size = max(max_size , (head - tail + 1)); // finding the new max if exist before moving head
+                head++;
             }
-            len=max(len,(r-l+1));
-            r++;
         }
-        return len;
+        return max_size;
     }
 };
