4ee04ad26457c4400d99b2af4615e91bf0bb4ef8
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-12 09:40:51 +0530

    Time: 243 ms (80.44%) | Memory: 35.3 MB (72.85%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
-        freq = {}
-        left = 0
-        max_len = 0
-        for right in range(len(nums)):
-            freq[nums[right]] = freq.get(nums[right], 0) + 1
-            while freq[nums[right]] > k:
-                freq[nums[left]] -= 1
-                left += 1
-            max_len = max(max_len, right - left + 1)
-        return max_len
+        i = j = 0
+        n = len(nums)
+        ans = 1
+        map = {}
+        while i < n:
+            map[nums[i]] = map.get(nums[i], 0) + 1
+            while map[nums[i]] > k:
+                map[nums[j]] -= 1
+                j += 1
+            ans = max(ans, i - j + 1)
+            i += 1
+        return ans
