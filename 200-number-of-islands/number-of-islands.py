3faff015e3725cc9aa29e37b359040a735be2ea6
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-12 15:13:21 +0530

    Time: 245 ms (53.44%) | Memory: 21.6 MB (71.91%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,20 +1,23 @@
 class Solution:
-    def dfs(self, grid, i, j):
-        m, n = len(grid), len(grid[0])
-        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] == '0':
-            return
-        grid[i][j] = '0'
-        self.dfs(grid, i + 1, j)
-        self.dfs(grid, i - 1, j)
-        self.dfs(grid, i, j + 1)
-        self.dfs(grid, i, j - 1)
-    
     def numIslands(self, grid: List[List[str]]) -> int:
-        m, n = len(grid), len(grid[0])
-        count = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    count += 1
-                    self.dfs(grid, i, j)
-        return count
+        if not grid:
+            return 0
+        islands=0
+        rows=len(grid)
+        cols=len(grid[0]) 
+        def dfs(r,c):
+            if r<0 or r>=rows or c<0  or c>=cols or grid[r][c]=='0':
+                return
+            grid[r][c]='0'
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]=='1':
+                    dfs(i,j)
+                    islands+=1
+        return islands                  
+
+        
