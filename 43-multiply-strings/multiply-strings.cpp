ac798a826e50adb8216e7a2647b8d1143795b7df
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-19 18:51:15 +0530

    Time: 3 ms (55.78%) | Memory: 10.9 MB (13.44%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        if (num1 == "0" || num2 == "0") return "0";        
+        if (num1 == "0" || num2 == "0") return "0";       
         vector<int> res(num1.size()+num2.size(), 0);        
         for (int i = num1.size()-1; i >= 0; i--) {
             for (int j = num2.size()-1; j >= 0; j--) {
@@ -13,7 +13,7 @@ public:
         int i = 0;
         string ans = "";
         while (res[i] == 0) i++;
-        while (i < res.size()) ans += to_string(res[i++]);       
+        while (i < res.size()) ans += to_string(res[i++]);        
         return ans;
     }
 };
