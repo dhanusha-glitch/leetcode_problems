4676e0f6d7eed02d1deff056590b5f7412961590
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-24 16:26:11 +0530

    Time: 113 ms (35.93%) | Memory: 61.2 MB (96.20%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -2,13 +2,16 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
         comb = []
+
         def backtrack(start):
             if len(comb) == k:
                 res.append(comb[:])
-                return           
+                return
+            
             for num in range(start, n + 1):
                 comb.append(num)
                 backtrack(num + 1)
                 comb.pop()
+
         backtrack(1)
         return res
