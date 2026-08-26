20f2ed6ad8c2574eb2d89337865008f50ad67021
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-26 19:21:46 +0530

    Time: 3 ms (50.13%) | Memory: 20.4 MB (69.84%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,7 +6,7 @@ class Solution:
                 return [True, 0]           
             left_balanced, left_height = dfs(root.left)
             right_balanced, right_height = dfs(root.right)
-            
+           
             is_balanced = left_balanced and right_balanced and abs(left_height - right_height) <= 1           
             return [is_balanced, 1 + max(left_height, right_height)]       
         return dfs(root)[0]
