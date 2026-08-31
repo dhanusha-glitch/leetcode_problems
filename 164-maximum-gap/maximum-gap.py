dc640c3cc372c6cb850ed32cc1dd013024225cd6
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-31 13:19:49 +0530

    Time: 246 ms (29.15%) | Memory: 34.6 MB (28.60%) - LeetSync

--- 164-maximum-gap/maximum-gap.py
+++ 164-maximum-gap/maximum-gap.py
@@ -1,16 +1,29 @@
-class Solution:
-    def maximumGap(self, nums: List[int]) -> int:
-        if len(nums) < 2: return 0
-        hi, lo, ans = max(nums), min(nums), 0
-        bsize = (hi - lo) // (len(nums) - 1) or 1
-        buckets = [[] for _ in range(((hi - lo) // bsize) + 1)]
-        for n in nums:
-            buckets[(n - lo) // bsize].append(n)
-        currhi = 0
-        for b in buckets:
-            if not len(b): continue
-            prevhi, currlo = currhi or b[0], b[0]
-            for n in b: 
-                currhi, currlo = max(currhi, n), min(currlo, n)
-            ans = max(ans, currlo - prevhi)
-        return ans
+class Solution(object):
+    def maximumGap(self, nums):
+        if len(nums) < 2:
+            return 0
+
+        min_num, max_num = min(nums), max(nums)
+        if min_num == max_num:
+            return 0
+
+        gap = max(1, (max_num - min_num) // (len(nums) - 1))
+        buckets = [[None, None] for _ in range((max_num - min_num) // gap + 1)]
+
+        for num in nums:
+            idx = (num - min_num) // gap
+            if buckets[idx][0] is None:
+                buckets[idx][0] = buckets[idx][1] = num
+            else:
+                buckets[idx][0] = min(buckets[idx][0], num)
+                buckets[idx][1] = max(buckets[idx][1], num)
+
+        max_gap = 0
+        prev_max = min_num
+
+        for bucket in buckets:
+            if bucket[0] is not None:
+                max_gap = max(max_gap, bucket[0] - prev_max)
+                prev_max = bucket[1]
+
+        return max_gap
