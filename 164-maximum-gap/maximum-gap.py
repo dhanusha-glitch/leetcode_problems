5fb21aaed107678ac9b6f06eb75d3adb0b245a5f
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-31 13:22:04 +0530

    Time: 398 ms (19.46%) | Memory: 58.5 MB (5.16%) - LeetSync

--- 164-maximum-gap/maximum-gap.py
+++ 164-maximum-gap/maximum-gap.py
@@ -1,24 +1,11 @@
-class Solution(object):
+class Solution:
     def maximumGap(self, nums):
-        if len(nums) < 2:
-            return 0
-        min_num, max_num = min(nums), max(nums)
-        if min_num == max_num:
-            return 0
-        gap = max(1, (max_num - min_num) // (len(nums) - 1))
-        buckets = [[None, None] for _ in range((max_num - min_num) // gap + 1)]
+        lo, hi, n = min(nums), max(nums), len(nums)
+        if n <= 2 or hi == lo: return hi - lo
+        B = defaultdict(list)
         for num in nums:
-            idx = (num - min_num) // gap
-            if buckets[idx][0] is None:
-                buckets[idx][0] = buckets[idx][1] = num
-            else:
-                buckets[idx][0] = min(buckets[idx][0], num)
-                buckets[idx][1] = max(buckets[idx][1], num)
-        max_gap = 0
-        prev_max = min_num
-
-        for bucket in buckets:
-            if bucket[0] is not None:
-                max_gap = max(max_gap, bucket[0] - prev_max)
-                prev_max = bucket[1]
-        return max_gap
+            ind = n-2 if num == hi else (num - lo)*(n-1)//(hi-lo)
+            B[ind].append(num)
+            
+        cands = [[min(B[i]), max(B[i])] for i in range(n-1) if B[i]]
+        return max(y[0]-x[1] for x,y in zip(cands, cands[1:]))
