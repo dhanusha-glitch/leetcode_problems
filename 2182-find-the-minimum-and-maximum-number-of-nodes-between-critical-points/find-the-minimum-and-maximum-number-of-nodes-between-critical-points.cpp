c3e794ca6eee63252600050b8f966ef0007fcaf2
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-31 13:05:52 +0530

    Time: 8 ms (26.86%) | Memory: 138.2 MB (5.02%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         vector<int> nums;
-
         while(head){
             nums.push_back(head->val);
             head = head->next;
         }
-
         vector<int> criticalPoints;
-
         int n = nums.size();
-
         for(int i = 1; i < n - 1; i++){
             if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]){
                 criticalPoints.push_back(i);
@@ -20,20 +16,14 @@ public:
                 criticalPoints.push_back(i);
             }
         }
-
         int m = criticalPoints.size();
-
         if(m < 2) return {-1, -1};
-
         int minDist = INT_MAX;
-
         int maxDist = criticalPoints[m - 1] - criticalPoints[0];
-
         for(int i = 1; i < m; i++){
             minDist = min(minDist,
                 criticalPoints[i] - criticalPoints[i - 1]);
         }
-
         return {minDist, maxDist};
     }
 };
