26e22d927ad20bfe27ec20ce3c85e4ba11104810
Dhanusha B <dhanushavalmiki@gmail.com>
2026-08-31 13:20:20 +0530

    Time: 237 ms (30.23%) | Memory: 34.8 MB (25.51%) - LeetSync

--- 164-maximum-gap/maximum-gap.py
+++ 164-maximum-gap/maximum-gap.py
@@ -2,14 +2,11 @@ class Solution(object):
     def maximumGap(self, nums):
         if len(nums) < 2:
             return 0
-
         min_num, max_num = min(nums), max(nums)
         if min_num == max_num:
             return 0
-
         gap = max(1, (max_num - min_num) // (len(nums) - 1))
         buckets = [[None, None] for _ in range((max_num - min_num) // gap + 1)]
-
         for num in nums:
             idx = (num - min_num) // gap
             if buckets[idx][0] is None:
@@ -17,7 +14,6 @@ class Solution(object):
             else:
                 buckets[idx][0] = min(buckets[idx][0], num)
                 buckets[idx][1] = max(buckets[idx][1], num)
-
         max_gap = 0
         prev_max = min_num
 
@@ -25,5 +21,4 @@ class Solution(object):
             if bucket[0] is not None:
                 max_gap = max(max_gap, bucket[0] - prev_max)
                 prev_max = bucket[1]
-
         return max_gap
