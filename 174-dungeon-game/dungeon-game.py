a1ba8adb734602aa3edc6c80c70bad2819deaf5b
Dhanusha B <dhanushavalmiki@gmail.com>
2026-09-01 11:01:20 +0530

    Time: 3 ms (81.20%) | Memory: 20.1 MB (35.84%) - LeetSync

--- 174-dungeon-game/dungeon-game.py
+++ 174-dungeon-game/dungeon-game.py
@@ -1,24 +1,11 @@
 class Solution:
-    def calculateMinimumHP(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])       
-        def isGood(initHealth):
-            dp = [[0] * n for _ in range(m)]
-            dp[0][0] = initHealth + grid[0][0]
-            for r in range(m):
-                for c in range(n):
-                    if r > 0 and dp[r-1][c] > 0:
-                        dp[r][c] = max(dp[r][c], dp[r-1][c] + grid[r][c])
-                    if c > 0 and dp[r][c-1] > 0:
-                        dp[r][c] = max(dp[r][c], dp[r][c-1] + grid[r][c])
-            return dp[m-1][n-1] > 0       
-        left = 1
-        right = 1000 * (m + n) + 1
-        ans = right
-        while left <= right:
-            mid = left + (right - left) // 2
-            if isGood(mid):
-                ans = mid
-                right = mid - 1
-            else:
-                left = mid + 1
-        return ans
+    def calculateMinimumHP(self, dungeon):
+        m, n = len(dungeon), len(dungeon[0])
+        dp = [[float("inf")]*(n+1) for _ in range(m+1)]
+        dp[m-1][n], dp[m][n-1] = 1, 1
+            
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                dp[i][j] = max(min(dp[i+1][j],dp[i][j+1])-dungeon[i][j],1)
+        
+        return dp[0][0]
